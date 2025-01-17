73ffed39f6ae7be7dd7107bcbadd9e4b196aad2e
Qlaramedbindestreg <u2470787@rts-365.dk>
2025-01-17 09:43:19 +0100

    add staged changes

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+     publicPath: '/Coffeeshop/'
   },
   devtool: 'source-map',
   module: {
